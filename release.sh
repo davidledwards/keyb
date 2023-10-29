4870e264754f6b271579943eb57408bce384fdbb
David Edwards <david.l.edwards@gmail.com>
2023-10-29 18:56:10 -0500

    fix shellcheck lint warnings

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [[ -z $(which gh) ]]; then
     echo "gh: GitHub CLI not found (see https://cli.github.com/)"
@@ -18,10 +18,10 @@ fi
 __VERSION=$($__BIN --version | cut -d " " -f 2)
 
 # Create tarball and checksum artifacts.
-__TAR=$__BIN_DIR/keyb-${__VERSION}-x86_64-unix.tar.gz
-tar -czf $__TAR -C $__BIN_DIR keyb
-shasum -a 256 $__TAR > $__TAR.sha256
+__TAR="$__BIN_DIR/keyb-${__VERSION}-x86_64-unix.tar.gz"
+tar -czf "$__TAR" -C "$__BIN_DIR keyb"
+shasum -a 256 "$__TAR" > "$__TAR.sha256"
 
 # Create actual release in GitHub
-gh release create --title "$__VERSION" --generate-notes "v$__VERSION" $__TAR*
+gh release create --title "$__VERSION" --generate-notes "v$__VERSION" "$__TAR"*
 echo "released $__VERSION"
