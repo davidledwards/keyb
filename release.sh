024941ff40853fb7fd4f66a17c210d44deef326c
David Edwards <david.l.edwards@gmail.com>
2024-07-23 13:52:19 -0500

    fix problem in cross-compilation naming

--- release.sh
+++ release.sh
@@ -6,19 +6,19 @@ if [[ -z $(which gh) ]]; then
 fi
 
 function build() {
-    local __TARGET="$1"
+    local __ARCH="$1"
+    local __TARGET="${__ARCH}-apple-darwin"
     local __BIN_DIR="$(pwd)/target/${__TARGET}/release"
     local __BIN="$__BIN_DIR/keyb"
 
     echo "$__TARGET: building target"
     cargo build --release --target=${__TARGET}
-
     if [ $? -ne 0 ]; then
         echo "$__BIN: build failure"
         exit 1
     fi
 
-    __TAR="$__BIN_DIR/keyb-${__VERSION}-$(uname -m)-unix.tar.gz"
+    __TAR="$__BIN_DIR/keyb-${__VERSION}-${__ARCH}-unix.tar.gz"
     tar -czf "$__TAR" -C "$__BIN_DIR" keyb
     shasum -a 256 "$__TAR" > "$__TAR.sha256"
 }
@@ -26,17 +26,16 @@ function build() {
 echo "detecting version"
 __VERSION=$(cargo run --release -- --version | cut -d " " -f 2)
 
-__TARGETS=("aarch64-apple-darwin" "x86_64-apple-darwin")
-__TARS=()
+__TARGETS=("aarch64" "x86_64")
+__FILES=()
 for __TARGET in "${__TARGETS[@]}"
 do
     build "$__TARGET"
-    __TARS+=("$__TAR" "$__TAR.sha256")
+    __FILES+=("$__TAR" "$__TAR.sha256")
 done
 
 # Create actual release in GitHub
-gh release create --title "$__VERSION" --generate-notes "v$__VERSION" ${__TARS[@]}
-
+gh release create --title "$__VERSION" --generate-notes "v$__VERSION" ${__FILES[@]}
 if [ $? -eq 0 ]; then
     echo "$__VERSION: release successful"
 else
